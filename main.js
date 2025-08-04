a6736af334a9a680cd22cd328d8774abffa815a5
Letícia de Alencar Fernandes <le.alefernandes@gmail.com>
2025-08-04 02:05:44 +0000

    CSS completo

---
+++ main.js
@@ -0,0 +1,9 @@
+function calcularSoma(event) {
+    event.preventDefault()
+
+    const num1 = document.getElementById("primeiro").valueAsNumber
+    const num2 = document.getElementById("segundo").valueAsNumber
+
+    const soma = num1 + num2
+    const p  = document.getElementById("resultado")
+}
